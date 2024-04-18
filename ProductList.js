98d4431975c8820377093957fffda67805da663b
ajbuttowski25 <160390781+ajbuttowski25@users.noreply.github.com>
2024-04-18 15:44:46 +0800

    ProductList.js

--- ProductList.js
+++ ProductList.js
@@ -12,19 +12,5 @@ const ProductList = ({ products, addToCart }) => {
   };
         </div>
     );
-    return (
-        <div>
-            <h1>Product List</h1>
-            <ul>
-                <li>
-                    <Link to="/product/1">Product 1</Link>
-                </li>
-                <li>
-                    <Link to="/product/2">Product 2</Link>
-                </li>
-            </ul>
-        </div>
-    );
-};
 
 export default ProductList;
